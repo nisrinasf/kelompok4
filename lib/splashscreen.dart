e81c95b14fd5c397cb17bec47a6d914284f8c214
unknown <nisrinashofwahfirdaus@gmail.com>
2022-09-06 08:24:57 +0700

    Membuat splashscreen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  Splashscreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<Splashscreen> {
+  void intState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  @override
+  widget build(BuildContext context) {}
+}
